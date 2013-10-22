ea7cb86b5eaf120e426a48a8fc20f5b7ed7876df
Alexander Leibzon <alexander.leibzon@gmail.com>
2013-10-22 12:21:12 +0300

    initial commit

---
+++ ch-ssh.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+PS3='Choose the server to connect to: '
+
+#get the available nodes list
+options=($(knife node list | grep $1))
+select opt in "${options[@]}"
+do
+    case $opt in
+        *) 
+	echo 'connecting to '$opt
+	ssh $(whoami)@$(knife node show $opt | grep IP\: | cut -d':' -f2 | tr -d ' ')
+	break
+	;;
+    esac
+done
